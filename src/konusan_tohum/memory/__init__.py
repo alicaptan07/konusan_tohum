a9ee3b70ea2445675d1c0e422dd45243be660d0a
alicaptan07 <alicaptan07@gmail.com>
2025-10-02 12:07:07 +0300

    Expose memory updater APIs

--- src/konusan_tohum/memory/__init__.py
+++ src/konusan_tohum/memory/__init__.py
@@ -1,1 +1,17 @@
-"""konusan_tohum.memory package."""
+"""Public interface for the :mod:`konusan_tohum.memory` package."""
+
+from .memory_updater import (  # noqa: F401
+    auto_update_memory,
+    get_user_memory,
+    load_user_memory,
+    save_memory,
+    update_memory,
+)
+
+__all__ = [
+    "auto_update_memory",
+    "get_user_memory",
+    "load_user_memory",
+    "save_memory",
+    "update_memory",
+]
